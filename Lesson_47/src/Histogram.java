96cd2f044514e1298b8aa32c6c8670519ec9a219
Nicolas F. Arisi Fernandez <nicoarisi339@gmail.com>
2023-10-18 21:44:33 -0400

    fixed code, still no * thing

--- Lesson_47/src/Histogram.java
+++ Lesson_47/src/Histogram.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Histogram {
 
 	public static void main(String[] args) throws FileNotFoundException {
+		//read text file
 		Scanner sf = new Scanner(new File("HistogramData.txt"));
 	    int maxIndx = -1;
 	    String text[] = new String[16]; // To be safe, declare more than we need
@@ -10,14 +11,17 @@ public class Histogram {
 		    maxIndx++;
 		    text[maxIndx] = sf.nextLine();
 	    }sf.close();
+	    //convert the text line to words
 	    String words[]=text[0].split(", ");
-	    /*for(int x=0; x<words.length;x++) {
+	    for(int x=0; x<words.length;x++) {
 	    	System.out.println(words[x]);
-	    }*/
+	    }
+	    //start map
+	    //Numbers part
 	    Map<String, Integer> s1 = new TreeMap<String, Integer>();
 	    for(int y=0;y<words.length;y++) {
 	    	if(s1.containsKey(words[y])) {
-	    		s1.replace(words[y], s1.get(words[y]+1));
+	    		s1.replace(words[y], s1.get(words[y])+1);
 	    	}else {
 	    		s1.put(words[y], 1);
 	    	}
@@ -25,9 +29,18 @@ public class Histogram {
 	    Set s2 = s1.keySet();
 	    Iterator<String> iter = s2.iterator();
 	    while(iter.hasNext()){
+	    	String key = iter.next();
+	    	Integer val = s1.get(key);
+	    	System.out.println(key + ", " + val);
+	    }
+	    //those * map
+	    Set s3 = s1.keySet();
+	    Iterator<String> iter2 = s3.iterator();
+	    while(iter.hasNext()){
 	    	Object key = iter.next();
 	    	Object val = s1.get(key);
-	    	System.out.println(key + " " + val);
+	    	String[] Astrixes = {"*", "**", "***"};
+	    	System.out.println(key + ", " + Astrixes[(int)val-1]);
 	    }
 	}
 
